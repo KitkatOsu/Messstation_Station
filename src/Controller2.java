2cfdc62a8237326e6ff2a7a2d98ce7d0413089ab
h-almas <helmut.almas@gmail.com>
2021-06-23 20:47:05 +0200

    deleting more things because flo forgot to do it himself again tchtchtch

--- src/Controller2.java
+++ src/Controller2.java
@@ -15,47 +15,7 @@ public class Controller2 {
     private URL location;
 
     @FXML
-    private LineChart<?, ?> lineChart1;
-
-    @FXML
-    private BarChart<?, ?> barChart1;
-
-    @FXML
-    private LineChart<?, ?> lineChart2;
-
-    @FXML
-    private TextField chartText1;
-
-    @FXML
-    private TextField chartText2;
-
-    @FXML
-    private TextField barChartText1;
-
-    @FXML
-    private LineChart<?, ?> chartText3;
-
-    @FXML
-    private Button Tab1;
-
-    @FXML
-    private Button Tab2;
-
-    @FXML
-    private Button Tab3;
-
-    @FXML
     void initialize() {
-        assert lineChart1 != null : "fx:id=\"lineChart1\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert barChart1 != null : "fx:id=\"barChart1\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert lineChart2 != null : "fx:id=\"lineChart2\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert chartText1 != null : "fx:id=\"chartText1\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert chartText2 != null : "fx:id=\"chartText2\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert barChartText1 != null : "fx:id=\"barChartText1\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert chartText3 != null : "fx:id=\"chartText3\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert Tab1 != null : "fx:id=\"Tab1\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert Tab2 != null : "fx:id=\"Tab2\" was not injected: check your FXML file 'diagrams.fxml'.";
-        assert Tab3 != null : "fx:id=\"Tab3\" was not injected: check your FXML file 'diagrams.fxml'.";
 
     }
 }
